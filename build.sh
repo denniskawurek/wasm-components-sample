f4a2c8b7cd9a5993a3eeea3598e861b3e2fd6f13
Dennis Kawurek <dennis.kawurek@hotmail.de>
2024-04-23 19:30:02 +0200

    remove mv step.

--- build.sh
+++ build.sh
@@ -3,6 +3,4 @@
 (cd string-length && cargo component build --release)
 (cd app && cargo component build --release)
 
-#mv string_length/target/wasm32-wasi/release/string_length.wasm string_length/target/wasm32-wasi/release/string-length.wasm
-
 (wasm-tools compose app/target/wasm32-wasi/release/app.wasm -d string-length/target/wasm32-wasi/release/string-length.wasm -o out.wasm)
