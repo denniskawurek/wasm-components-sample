857839663c379acb7399c82d83f4caee86cc8540
Dennis Kawurek <dennis.kawurek@hotmail.de>
2024-04-24 19:50:10 +0200

    Cleanup

--- app/src/main.rs
+++ app/src/main.rs
@@ -4,13 +4,13 @@ mod bindings;
 use crate::bindings::dkwr::stringoperations::len::len;
 
 fn main() {
-    let args: Vec<String> = std::env::args().collect(); // Collect command line arguments into a vector of strings
+    let args: Vec<String> = std::env::args().collect();
     if args.len() != 2 {
         println!("Usage: {} <string>", args[0]);
         return;
     }
 
-    let input_string = &args[1]; // Extract the input string from command line arguments
+    let input_string = &args[1];
 
     let result = len(input_string);
     println!("{}", result);
