be447221d66fee18e87803500634da51abe5c917
Dennis Kawurek <dennis.kawurek@hotmail.de>
2024-04-24 19:48:32 +0200

    App collects arguments.

--- app/src/main.rs
+++ app/src/main.rs
@@ -4,6 +4,14 @@ mod bindings;
 use crate::bindings::dkwr::stringoperations::len::len;
 
 fn main() {
-    let result = len("d");
-    println!("{result}");
+    let args: Vec<String> = std::env::args().collect(); // Collect command line arguments into a vector of strings
+    if args.len() != 2 {
+        println!("Usage: {} <string>", args[0]);
+        return;
+    }
+
+    let input_string = &args[1]; // Extract the input string from command line arguments
+
+    let result = len(input_string);
+    println!("{}", result);
 }
